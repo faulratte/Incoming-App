7f6b0be9e78393b26636e98be5137f68ce814494
Peter Braun <mail@peter-braun.org>
2015-06-26 14:09:20 +0200

    Changed base URIs to show only latest (valid) news and events

--- app/src/main/java/fhws/marcelgross/incoming/UrlHandler.java
+++ app/src/main/java/fhws/marcelgross/incoming/UrlHandler.java
@@ -6,8 +6,8 @@ package fhws.marcelgross.incoming;
 public interface UrlHandler {
 
     String LINKSURL = "http://backend.applab.fhws.de:8080/incoming/api/index";
-    String NEWSURL = "http://backend.applab.fhws.de:8080/incoming/api/news";
-    String EVENTLINK = "http://backend.applab.fhws.de:8080/incoming/api/events";
+    String NEWSURL = "http://backend.applab.fhws.de:8080/incoming/api/news/latest";
+    String EVENTLINK = "http://backend.applab.fhws.de:8080/incoming/api/events/latest";
     String CONTACTURL = "http://backend.applab.fhws.de:8080/incoming/api/person";
     String NAVILINK = "http://backend.applab.fhws.de:8080/incoming/api/navi";
 }
